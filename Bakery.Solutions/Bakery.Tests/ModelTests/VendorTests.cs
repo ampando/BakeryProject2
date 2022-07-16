5dfa67debf28103556bd1cd12638831b3e2aad45
Adrienne <100542795+ampando@users.noreply.github.com>
2022-07-15 17:19:54 -0700

    failing test for name of vendor.

--- Bakery.Solutions/Bakery.Tests/ModelTests/VendorTests.cs
+++ Bakery.Solutions/Bakery.Tests/ModelTests/VendorTests.cs
@@ -18,8 +18,15 @@ namespace Bakery.Tests
     Vendor newVendor = new Vendor("Test Vendor");
     Assert.AreEqual(typeof(Vendor), newVendor.GetType());
   }
-  
-  
-  
+  [TestMethod]
+  public void GetName_ReturnsName_String()
+  {
+    string name = "Test Vendor";
+    Vendor newVendor = new Vendor(name);
+
+    string result = newVendor.Name;
+
+    Assert.AreEqual(name, result);
+  }
   }
 }
