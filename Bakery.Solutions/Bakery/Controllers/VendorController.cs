895d81e80b4f1e7c1184c51354f8745ef8ae195e
Adrienne <100542795+ampando@users.noreply.github.com>
2022-07-15 18:40:03 -0700

    create order controller.

---
+++ Bakery.Solutions/Bakery/Controllers/VendorController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+  public class VendorController : Controllers
+  {
+    
+  }
+}
