aa19390a796be278e938e0e65218a2a6f5d2c150
Adrienne <100542795+ampando@users.noreply.github.com>
2022-07-15 18:41:48 -0700

    create vendorcontroller.

--- Bakery.Solutions/Bakery/Controllers/VendorController.cs
+++ Bakery.Solutions/Bakery/Controllers/VendorController.cs
@@ -7,6 +7,11 @@ namespace Bakery.Controllers
 {
   public class VendorController : Controllers
   {
-    
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
   }
 }
