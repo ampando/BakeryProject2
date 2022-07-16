d2d76967cf5e40afcce857eb2efa3989dacd6eae
Adrienne <100542795+ampando@users.noreply.github.com>
2022-07-15 18:03:25 -0700

    failing test for order matching vendor.

--- Bakery.Solutions/Bakery.Tests/ModelTests/VendorTests.cs
+++ Bakery.Solutions/Bakery.Tests/ModelTests/VendorTests.cs
@@ -9,59 +9,72 @@ namespace Bakery.Tests
   public class VendorTest : IDisposable
   {
     public void Dispose()
-  {
-    Vendor.ClearAll();
-  }
-  [TestMethod]
-  public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
-  {
-    Vendor newVendor = new Vendor("Test Vendor");
-    Assert.AreEqual(typeof(Vendor), newVendor.GetType());
-  }
-  [TestMethod]
-  public void GetName_ReturnsName_String()
-  {
-    string name = "Test Vendor";
-    Vendor newVendor = new Vendor(name);
+    {
+      Vendor.ClearAll();
+    }
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Test Vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
 
-    string result = newVendor.Name;
+      string result = newVendor.Name;
 
-    Assert.AreEqual(name, result);
-  }
-  [TestMethod]
-  public void GetId_ReturnsVendorId_Int()
-  {
-    string name = "Test Vendor";
-    Vendor newVendor = new Vendor(name);
+      Assert.AreEqual(name, result);
+    }
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
 
-    int result = newVendor.Id;
+      int result = newVendor.Id;
 
-    Assert.AreEqual(1, result);
-  }
-  [TestMethod]
-  public void GetAll_ReturnsAllVendorObjects_VendorList()
-  {
-    string name1 = "Test Vendor One";
-    string name2 = "Test Vendor Two";
-    Vendor newVendor1 = new Vendor(name1);
-    Vendor newVendor2 = new Vendor(name2);
-    List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      Assert.AreEqual(1, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string name1 = "Test Vendor One";
+      string name2 = "Test Vendor Two";
+      Vendor newVendor1 = new Vendor(name1);
+      Vendor newVendor2 = new Vendor(name2);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
 
-    List<Vendor> result = Vendor.GetAll();
+      List<Vendor> result = Vendor.GetAll();
 
-    CollectionAssert.AreEqual(newList, result);
-  }
-  [TestMethod]
-  public void Find_ReturnsCorrectVendor_Vendor()
-  {
-    string name1 = "Test Vendor One";
-    string name2 = "Test Vendor Two";
-    Vendor newVendor1 = new Vendor(name1);
-    Vendor newVendor2 = new Vendor(name2);
+      CollectionAssert.AreEqual(newList, result);
+    }
+    [TestMethod]
+    public void Find_ReturnsCorrectVendor_Vendor()
+    {
+      string name1 = "Test Vendor One";
+      string name2 = "Test Vendor Two";
+      Vendor newVendor1 = new Vendor(name1);
+      Vendor newVendor2 = new Vendor(name2);
 
-    Vendor result = Vendor.Find(2);
+      Vendor result = Vendor.Find(2);
 
-    Assert.AreEqual(newVendor2, result);   
-  }
+      Assert.AreEqual(newVendor2, result);   
+    }
+    [TestMethod]
+    public void AddOrder_AssociatesOrderWithVendor_OrderList()
+    {
+      Order newOrder = new Order("Test Order", "Test Description", 50, "Test Date", "Test Frequency");
+      List<Order> newList = new List<Order> { newOrder };
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+      newVendor.AddOrder(newOrder);
+
+      List<Order> result = newVendor.Orders;
+
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
--- Bakery.Solutions/Bakery/Models/Vendor.cs
+++ Bakery.Solutions/Bakery/Models/Vendor.cs
@@ -3,30 +3,33 @@ using System.Collections.Generic;
 namespace Bakery.Models
 {
   public class Vendor
-{
-  private static List<Vendor> _instances = new List<Vendor> { };
-  public string Name { get; set; }
-  public List<Order> Orders { get; set; }
-  public int Id { get; }
-  public Vendor(string vendorName)
   {
-    Name = vendorName;
-    Orders = new List<Order>{};
-    _instances.Add(this);
-    Id = _instances.Count;
-  }
-  public static void ClearAll()
-  {
-    _instances.Clear();
-  }
-  public static List<Vendor> GetAll()
-  {
-    return _instances;
-  }
-  public static Vendor Find(int id)
-  {
-    return _instances[id - 1];
-  }
-
+    private static List<Vendor> _instances = new List<Vendor> { };
+    public string Name { get; set; }
+    public List<Order> Orders { get; set; }
+    public int Id { get; }
+    public Vendor(string vendorName)
+    {
+      Name = vendorName;
+      //Orders = new List<Order>{};
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+    public static Vendor Find(int id)
+    {
+      return _instances[id - 1];
+    }
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
   }
 }
